9feb699d5b7dea46c4e6a588d05522b70c74c538
Jason Park <jjpark987@gmail.com>
2024-01-11 13:04:41 -0800

    Add validation for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, :username, presence: true, uniqueness: true
+    validates :email, format: { 
+        with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/, 
+        message: "is not in the correct format." 
+    }
 end
